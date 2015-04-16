803b509e9a3e8940a6bd71161e173cfe3a7f9a1d
Nick Husher <nick.husher@faraday.io>
2015-04-16 10:48:37 -0400

    Add index.js so we can easily browserify this thing

---
+++ index.js
@@ -0,0 +1,5 @@
+require('./featureFlag.app');
+require('./featureFlag.directive');
+require('./featureFlagOverrides.directive');
+require('./featureFlagOverrides.service');
+require('./featureFlags.service.js');
